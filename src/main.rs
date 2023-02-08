c5020f5083e13d04df009a71b6eed72acbdd3b21
caplincapture <leuping@gmail.com>
2023-02-07 17:53:19 -0800

    encoder finish and parallel write begin

--- src/main.rs
+++ src/main.rs
@@ -149,10 +149,15 @@ use std::fs::File;
 
 /// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
 /// file named `filename`.
+
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
     -> Result<(), std::io::Error>
-{
-    
+{   
+    let output = File::create(filename)?;
+    let mut encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::RGBA(2))?;
+
+    Ok(())
 }
 
 use std::env;
